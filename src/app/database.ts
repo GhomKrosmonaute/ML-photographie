a81ce05e46e2a156f23eabbfe7e632b5d31d2dbe
CamilleAbella <camille.abella@hotmail.fr>
2021-12-05 15:09:43 +0100

    update db setup

--- src/app/database.ts
+++ src/app/database.ts
@@ -1,10 +1,15 @@
 import knex from "knex"
 import path from "path"
+import fs from "fs"
+
+const dbPath = path.join(__dirname, "..", "..", "data")
+
+if(!fs.existsSync(dbPath)) fs.mkdirSync(dbPath)
 
 export const db = knex({
   client: "sqlite3",
   useNullAsDefault: true,
   connection: {
-    filename: path.join(__dirname, "..", "..", "data", "data.sql"),
+    filename: path.join(dbPath, "data.sql"),
   },
 })
